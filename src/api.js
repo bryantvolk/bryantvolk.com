1a6d8b1dcfa81359c235b4fee465392c7dc72a56
bryant volk <bryantvolk96@gmail.com>
2018-03-12 23:18:51 -0400

    wow i finally got the right reverse proxy url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // here is a wrapper for api calls
 import axios from 'axios';
 // url for java server api
-const baseURL = 'https://bryantvolk.com:8080';
+const baseURL = 'https://bryantvolk.com/api';
 
 export function getReviews() {
   return axios.get(baseURL + '/reviews')
